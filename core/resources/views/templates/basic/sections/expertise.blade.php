5dcca6afb37d0d26e5b3cadfcabef11679f4e936
Ayonislam12 <md.mehedihasaniubat@gmail.com>
2023-08-10 02:03:21 +0600

    My Expertise Section Done

--- core/resources/views/templates/basic/sections/expertise.blade.php
+++ core/resources/views/templates/basic/sections/expertise.blade.php
@@ -20,7 +20,7 @@
                         <div class="service-box-item-content">
                             <h4>{{ @$item->data_values->title }}</h4>
                             <p>{{shortDescription(strip_tags(@$item->data_values->details,200))}}</p>
-                            <a href="javascript:void(0)" class="detailsBtn" data-title ="{{ @$item->data_values->title }}" data-img="{{getImage('assets/images/frontend/expertise/'.@$item->data_values->details_img)}}" data-details ="{{ strip_tags(@$item->data_values->details) }}">{{ __("Read More") }}</a>
+                            <a href="javascript:void(0)" class="detailsBtn" data-title ="{{ @$item->data_values->title }}" data-img="{{getImage('assets/images/frontend/expertise/'.@$item->data_values->details_img)}}" data-details ="{{ $item->data_values->details }}">{{ __("Read More") }}</a>
                         </div>
                         </div>
                     </div>
@@ -39,7 +39,7 @@
           </div>
           <div class="modal-body service-model-content">
              <figure class="mb-0">
-                <img src="" alt="ui-ux-model-img " class="img-fluid _image">
+                <img src="" alt="details-image" class="img-thumbnail _image rounded">
              </figure>
              <h4 class="_title">--</h4>
              <p class="mb-md-4 mb-2 _details text-justify" >--</p>
@@ -58,7 +58,7 @@
             var image = $(this).data('img');
             $('._image').attr('src',image);
             var details = $(this).data('details');
-            $('._details').text(details); // Get the text
+            $('._details').html(details);
             modal.modal('show');
         });
     })(jQuery);
